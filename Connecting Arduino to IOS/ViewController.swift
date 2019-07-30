87e26baffa55270384b8f470afefeca1b4123516
Brian Chan <brian.534211@gmail.com>
2019-07-30 17:37:07 -0400

    Initial Commit

---
+++ Connecting Arduino to IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Connecting Arduino to IOS
+//
+//  Created by Brian Chan on 2019-07-30.
+//  Copyright © 2019 Brian Chan and Sigil Wen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
